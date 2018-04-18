81b3b81989d0f48bf8ba55ae69c6da41b4b0745c
Scott Sherman <scott@loki3.com>
2018-04-18 15:46:21 -0700

    get rid of placeholder files

--- src/Src.ts
+++
@@ -1,4 +0,0 @@
-namespace Flexagonator {
-  export const x = 42;
-  console.log("hello");
-}
--- test/Test.ts
+++
@@ -1,7 +0,0 @@
-namespace Flexagonator {
-  describe('describe', () => {
-    it('should be 42', () => {
-      expect(x).toEqual(42);
-    });
-  });
-}
