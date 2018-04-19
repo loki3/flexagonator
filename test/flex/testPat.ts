4bbd71b080fee20c475a13d3fc904d88aab5194e
Scott Sherman <scott@loki3.com>
2018-04-19 11:48:34 -0700

    test makeFlipped

--- test/flex/testPat.ts
+++ test/flex/testPat.ts
@@ -7,4 +7,14 @@ namespace Flexagonator {
       expect(areEqual(result, original)).toBeTruthy();
     });
   });
+
+  describe('makeFlipped', () => {
+    it('should properly flip a pat', () => {
+      const original = [1, [[-2, 3], 4]];
+      const expected = [[-4, [-3, 2]], -1];
+      const pat = makePat(original);
+      const result = pat.makeFlipped().getAsLeafTree();
+      expect(areEqual(result, expected)).toBeTruthy();
+    });
+  });
 }
