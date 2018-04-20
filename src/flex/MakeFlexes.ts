9bc9923b605ea176064314996690db94c3e55b15
Scott Sherman <scott@loki3.com>
2018-04-20 12:49:44 -0700

    add a bunch more flexes for hexas

--- src/flex/MakeFlexes.ts
+++ src/flex/MakeFlexes.ts
@@ -26,6 +26,33 @@ namespace Flexagonator {
       flexes["S"] = makeFlex("pyramid shuffle",
         [[1, 2], 3, 4, 5, [[[6, 7], 8], 9], 10],
         [[1, [8, [2, -10]]], 3, 4, 5, [7, 9], -6]) as Flex;
+      flexes["V"] = makeFlex("v flex",
+        [1, [2, 3], [4, 5], 6, 7, [8, 9]],
+        [[3, -1], -2, -5, [-6, 4], [9, -7], -8]) as Flex;
+      flexes["F"] = makeFlex("flip flex",
+        [[1, 2], 3, 4, 5, [[6, 7], [8, 9]], 10],
+        [7, [[-1, 3], [10, -2]], 4, 5, 8, [-6, -9]]) as Flex;
+      flexes["St"] = makeFlex("silver tetra flex",
+        [[1, [2, 3]], 4, 5, 6, [7, [8, 9]], 10],
+        [2, [[-1, 4], -3], 5, 6, 8, [[-7, 10], -9]]) as Flex;
+      flexes["Lt"] = makeFlex("slot tuck flex",
+        [[[[1, 2], 3], 4], 5, 6, 7, [8, 9], 10],
+        [10, [2, 4], -1, 3, 5, [8, [6, [9, -7]]]]) as Flex;
+      flexes["Ltb"] = makeFlex("slot tuck bottom",
+        [[[1, [2, 3]], 4], 5, 6, 7, [8, 9], 10],
+        [-4, 1, -3, [-5, 2], [[7, -9], -6], [-10, 8]]) as Flex;
+      flexes["Ltb'"] = makeFlex("inverse slot tuck bottom",
+        [1, 2, 3, [4, 5], [[6, 7], 8], [9, 10]],
+        [[[2, [5, -3]], -1], -4, -8, .6, [10, -7], -9]) as Flex;
+      flexes["Bltt"] = makeFlex("back slot tuck top",
+        [[[1, 2], 10], [4, 3], 5, 6, [8, 7], 9],
+        [[-2, -1], -3, [-5, 4], [6, -7], -8, [10, -9]]) as Flex;
+      flexes["Tk"] = makeFlex("ticket flex",
+        [1, 2, 3, [4, 5], [[[6, 7], 8], 9], [10, 11]],
+        [6, [-9, -7], [-5, -4], -3, -2, [[11, -8], [-1, 10]]]) as Flex;
+      flexes["Lk"] = makeFlex("slot pocket",
+        [[[[1, 2], 3], 4], 5, 6, 7, [[[8, 9], 10], 11], 12],
+        [-8, [2, [10, [4, -12]]], -1, 3, 5, [9, [6, 11, -7]]]) as Flex;
     }
     return flexes;
   }
