6bdf64c3d34902733f6c70d7da2a2755a976ce31
Scott Sherman <scott@loki3.com>
2018-04-22 11:25:22 -0700

    adjust the hexahexa sample

--- src/flex/Samples.ts
+++ src/flex/Samples.ts
@@ -1,9 +1,9 @@
 namespace Flexagonator {
 
   export const hexaHexaLeafTree: LeafTree = [
-    [1, -2], [[4, -3], [6, -5]],
-    [7, -8], [[10, -9], [12, -11]],
-    [13, -14], [[16, -15], [18, -17]],
+    [1, -18], [[4, -5], [2, -3]],
+    [7, -6], [[10, -11], [8, -9]],
+    [13, -12], [[16, -17], [14, -15]],
   ];
 
   const hh1 = { label: "1", color: 0x0000ff };
@@ -15,23 +15,23 @@ namespace Flexagonator {
 
   export const hexaHexaProperties: LeafProperties[] = [
     { front: hh1, back: hh4 },  // 1
-    { front: hh2, back: hh4 },  // 2
+    { front: hh2, back: hh5 },  // 2
     { front: hh3, back: hh5 },  // 3
-    { front: hh1, back: hh5 },  // 4
+    { front: hh1, back: hh6 },  // 4
     { front: hh2, back: hh6 },  // 5
-    { front: hh3, back: hh6 },  // 6
+    { front: hh3, back: hh4 },  // 6
     { front: hh1, back: hh4 },  // 7
-    { front: hh2, back: hh4 },  // 8
+    { front: hh2, back: hh5 },  // 8
     { front: hh3, back: hh5 },  // 9
-    { front: hh1, back: hh5 },  // 10
+    { front: hh1, back: hh6 },  // 10
     { front: hh2, back: hh6 },  // 11
-    { front: hh3, back: hh6 },  // 12
+    { front: hh3, back: hh4 },  // 12
     { front: hh1, back: hh4 },  // 13
-    { front: hh2, back: hh4 },  // 14
+    { front: hh2, back: hh5 },  // 14
     { front: hh3, back: hh5 },  // 15
-    { front: hh1, back: hh5 },  // 16
+    { front: hh1, back: hh6 },  // 16
     { front: hh2, back: hh6 },  // 17
-    { front: hh3, back: hh6 },  // 18
+    { front: hh3, back: hh4 },  // 18
   ]
 
 }
