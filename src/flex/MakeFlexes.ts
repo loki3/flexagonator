f40b417cc6e277091a5518c42b67f7fdf6764374
Scott Sherman <scott@loki3.com>
2018-04-30 18:25:30 -0700

    add the 'back slot tuck bottom' flex
    catchy name

--- src/flex/MakeFlexes.ts
+++ src/flex/MakeFlexes.ts
@@ -91,6 +91,9 @@ namespace Flexagonator {
     flexes["Bltt"] = makeFlex("back slot tuck top",
       [[[1, 2], 10], [4, 3], 5, 6, [8, 7], 9],
       [[-2, -1], -3, [-5, 4], [7, -6], -8, [10, -9]], FlexRotation.CounterMirror) as Flex;
+    flexes["Bltb"] = makeFlex("back slot tuck bottom",
+      [[[2, -3], -1], [5, -4], 6, 7, [[-9, 10], 8], 11],
+      [2, [-4, 3], -5, [[7, -8], -6], -9, [[11, 1], -10]], FlexRotation.CounterMirror) as Flex;
     flexes["Tk"] = makeFlex("ticket flex",
       [1, 2, 3, [4, 5], [[[6, 7], 8], 9], [10, 11]],
       [6, [-9, -7], [-5, -4], -3, -2, [[11, -8], [-1, 10]]], FlexRotation.None) as Flex;
