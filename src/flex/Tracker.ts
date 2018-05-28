6140b530bd2b1fa48fe6c9a6b6642bdefb8213bc
Scott Sherman <scott@loki3.com>
2018-05-28 07:44:45 -0700

    normalize the leaves for the tracker
    put the lowest leaf number first so it's easy to normalize - this allows
    it to recognize flexagons that have been flipped and rotated

--- src/flex/Tracker.ts
+++ src/flex/Tracker.ts
@@ -22,36 +22,81 @@ namespace Flexagonator {
 
     // returns which state we have, or null if we haven't seen it before
     private getIndex(outside: Outside): number | null {
-      for (var i in this.outsides) {
+      for (var i = 0; i < this.outsides.length; i++) {
         const thisOutside = this.outsides[i];
         if (thisOutside.isEqualTo(outside)) {
-          return Number.parseInt(i);
+          return i;
         }
       }
       return null;
     }
   }
 
+
   // represents the leaves visible on the outside of a flexagon
+  // normalized, so the 1st top leaf is the lowest value
   class Outside {
     readonly top: number[];
     readonly bottom: number[];
 
     constructor(top: number[], bottom: number[]) {
-      this.top = top;
-      this.bottom = bottom;
+      const where = Outside.findLowest(top, bottom);
+      if (where.onTop) {
+        this.top = this.rotate(where.index, top);
+        this.bottom = this.rotate(where.index, bottom);
+      } else {
+        const index = top.length - where.index - 1;
+        this.top = this.rotate(index, bottom.reverse());
+        this.bottom = this.rotate(index, top.reverse());
+      }
     }
 
     isEqualTo(other: Outside): boolean {
       for (var i in this.top) {
-        if (this.top[i] != other.top[i]) {
+        if (this.top[i] !== other.top[i]) {
           return false;
         }
-        if (this.bottom[i] != other.bottom[i]) {
+        if (this.bottom[i] !== other.bottom[i]) {
           return false;
         }
       }
       return true;
+    }
+
+    // search for the lowest number among top & bottom,
+    // (where <0 simply represents the backside, so we flip the sign)
+    // return { index: <pat #>, onTop: <true|false> }
+    private static findLowest(top: number[], bottom: number[]) {
+      var lowest: number | null = null;
+      var index = 0;
+      var onTop = true;
+      for (var i = 0; i < top.length; i++) {
+        const theTop = Math.abs(top[i]);
+        if (lowest === null || theTop < lowest) {
+          lowest = theTop;
+          index = i;
+          onTop = true;
+        }
+        const theBottom = Math.abs(bottom[i]);
+        if (lowest === null || theBottom < lowest) {
+          lowest = theBottom;
+          index = i;
+          onTop = false;
+        }
+      }
+      return { index: index, onTop: onTop };
+    }
+
+    // e.g. (2, [a,b,c,d]) -> [c,d,a,b]
+    private rotate(index: number, nums: number[]): number[] {
+      if (index === 0) {
+        return nums;
+      }
+      var rotated: number[] = [];
+      for (var i = 0; i < nums.length; i++) {
+        rotated[i] = nums[(i + index) % nums.length];
+      }
+      return rotated;
     }
   }
 
--- test/flex/testTracker.ts
+++ test/flex/testTracker.ts
@@ -27,4 +27,27 @@ namespace Flexagonator {
     });
   });
 
+
+  describe('Tracker', () => {
+    it('should track flexagons that are rotated & flipped', () => {
+      const tracker = new Tracker();
+      expect(tracker.getTotalStates()).toBe(0);
+
+      // add first flexagon
+      const flexagon = makeFlexagon([[1, 2], 3, [4, 5], 6]) as Flexagon;
+      expect(tracker.findMaybeAdd(flexagon)).toBe(null);
+      expect(tracker.getTotalStates()).toBe(1);
+
+      // add rotated version
+      const rotated = makeFlexagon([[4, 5], 6, [1, 2], 3]) as Flexagon;
+      expect(tracker.findMaybeAdd(rotated)).toBe(0);
+      expect(tracker.getTotalStates()).toBe(1);
+
+      // add flipped & rotated version
+      const flipped = makeFlexagon([-3, [-2, -1], -6, [-5, -4]]) as Flexagon;
+      expect(tracker.findMaybeAdd(flipped)).toBe(0);
+      expect(tracker.getTotalStates()).toBe(1);
+    });
+  });
+
 }
