90852d23b9ef3e0f7f3981a72500d8ec81df1d9d
Scott Sherman <scott@loki3.com>
2018-06-02 22:02:01 -0700

    make RelativeFlex info accessible

--- src/analyze/RelativeFlex.ts
+++ src/analyze/RelativeFlex.ts
@@ -6,8 +6,8 @@ namespace Flexagonator {
     // 'where' represents > and ^ used before the given flex
     // >0 for the number of >'s; <0 is a ^ followed by (where-1) >'s
     private readonly where: number;
-    private readonly flex: string;
-    private readonly toState: number;
+    readonly flex: string;
+    readonly toState: number;
 
     constructor(rights: number, over: boolean, flex: string, toState: number) {
       this.where = over ? -rights - 1 : rights;
@@ -15,12 +15,20 @@ namespace Flexagonator {
       this.toState = toState;
     }
 
+    getRights(): number {
+      return this.where > 0 ? this.where : -1 - this.where;
+    }
+
+    shouldTurnOver(): boolean {
+      return this.where < 0;
+    }
+
     toString(): string {
       var str = this.toState + '(';
-      if (this.where < 0) {
+      if (this.shouldTurnOver()) {
         str += '^';
       }
-      const rights = this.where > 0 ? this.where : -1 - this.where;
+      const rights = this.getRights();
       for (var i = 0; i < rights; i++) {
         str += '>';
       }
