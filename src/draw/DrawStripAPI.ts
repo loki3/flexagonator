d4665d6850044015a1dcff4b33d2570a814a3066
Scott Sherman <scott@loki3.com>
2018-06-10 09:01:36 -0700

    extract FlexagonManager from drawUnfolded

--- src/draw/DrawStripAPI.ts
+++ src/draw/DrawStripAPI.ts
@@ -1,5 +1,12 @@
 namespace Flexagonator {
 
+  // information about how the display properties of a given unfolded flexagon strip
+  export interface DrawStripObjects {
+    readonly flexagon: Flexagon;
+    readonly angleInfo: FlexagonAngles;
+    readonly leafProps: PropertiesForLeaves;
+  }
+
   export enum StripContent {
     FoldingLabels,  // put everything on one-side, use labels that indicate folding order
     FoldingAndIds,  // FoldingLabels plus ids
@@ -16,22 +23,32 @@ namespace Flexagonator {
     readonly scale?: number;
   }
 
+  // draw an unfolded flexagon strip
   export function drawUnfolded(canvasId: string, fm: FlexagonManager, options: DrawStripOptions) {
+    const objects = {
+      flexagon: fm.flexagon,
+      angleInfo: fm.getAngleInfo(),
+      leafProps: fm.leafProps,
+    };
+    return drawUnfoldedObjects(canvasId, objects, options);
+  }
+
+  function drawUnfoldedObjects(canvasId: string, objects: DrawStripObjects, options: DrawStripOptions) {
     const output: HTMLCanvasElement = document.getElementById(canvasId) as HTMLCanvasElement;
     const ctx = output.getContext("2d") as CanvasRenderingContext2D;
     ctx.clearRect(0, 0, 800, 600);
 
-    const unfolded = unfold(fm.flexagon.getAsLeafTrees());
+    const unfolded = unfold(objects.flexagon.getAsLeafTrees());
     if (isTreeError(unfolded)) {
       console.log("error unfolding flexagon");
       console.log(unfolded);
       return;
     }
 
-    const angles = fm.getAngleInfo().getUnfoldedAngles(fm.flexagon, unfolded);
+    const angles = objects.angleInfo.getUnfoldedAngles(objects.flexagon, unfolded);
     const leaflines = leafsToLines(unfolded, toRadians(angles[0]), toRadians(angles[1]));
     const leaflinesSubset = sliceLeafLines(leaflines, options.start, options.end);
-    drawStrip(ctx, leaflinesSubset, options.content, fm.leafProps, options.scale);
+    drawStrip(ctx, leaflinesSubset, options.content, objects.leafProps, options.scale);
   }
 
 }
