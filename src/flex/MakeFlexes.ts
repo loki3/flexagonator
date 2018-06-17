b06805183be804605b94bd8b174689c1250628db
Scott Sherman <scott@loki3.com>
2018-06-16 22:00:42 -0700

    add the slot half flex
    start by sliding leaves through the slot twice, just like many of the
    other hexa slots, but then you fold it into half a hexa & simply unfold
    it, rather than applying a pocket or tuck flex

--- src/flex/MakeFlexes.ts
+++ src/flex/MakeFlexes.ts
@@ -101,6 +101,9 @@ namespace Flexagonator {
     flexes["Lbtb"] = makeFlex("slot back tuck bottom",
       [[[2, -3], -1], [5, -4], 6, 7, [[-9, 10], 8], 11],
       [2, [-4, 3], -5, [[7, -8], -6], -9, [[11, 1], -10]], FlexRotation.CounterMirror) as Flex;
+    flexes["Lh"] = makeFlex("slot half",
+      [[[1, [2, 3]], 4], 5, 6, 7, [[8, 9], 10], 11],
+      [[[11, -4], -9], 1, -3, [-5, 2], [[7, -10], -6], 8], FlexRotation.CounterMirror) as Flex;
     flexes["Lk"] = makeFlex("slot pocket",
       [[[[1, 2], 3], 4], 5, 6, 7, [[[8, 9], 10], 11], 12],
       [-8, [2, [10, [4, -12]]], -1, 3, 5, [9, [6, [11, -7]]]], FlexRotation.None) as Flex;
