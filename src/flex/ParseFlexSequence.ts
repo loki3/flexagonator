c41fdd0622853913fcda2783929256517e4f137f
Scott Sherman <scott@loki3.com>
2018-06-29 08:03:39 -0700

    add the ability to parse repeats in flex sequences, e.g. (AB)x2

--- src/flex/ParseFlexSequence.ts
+++ src/flex/ParseFlexSequence.ts
@@ -1,31 +1,105 @@
 namespace Flexagonator {
 
   export function parseFlexSequence(sequence: string): FlexName[] {
-    const names: FlexName[] = [];
-    let start = -1;
-    let i = 0;
-
+    const track = new TrackState(sequence);
     for (let c of sequence) {
-      if (c == ' ' || c === '>' || c === '<' || c === '^' || ('A' <= c && c <= 'Z')) {
-        // we're starting a new flex, so end the previous one
-        addFlex(names, sequence, start, i);
-        start = i;
-      }
-
-      i++;
+      track.step(c);
     }
-
-    addFlex(names, sequence, start, i);
-    return names;
+    track.done();
+    return track.getNames();
   }
 
-  function addFlex(names: FlexName[], sequence: string, start: number, end: number) {
-    if (start != -1) {
-      const substr = sequence.substring(start, end).trim();
-      if (substr.length > 0) {
-        names.push(makeFlexName(substr));
+  // track a (nested sequence) when we recurse
+  class SequenceState {
+    index: number;
+    start: number = -1;
+    lookingForNumber = false;
+    number = "";
+    names: FlexName[] = [];
+
+    constructor(index: number) { this.index = index; }
+
+    // expand (AB)x2 into ABAB
+    getFlexes(): FlexName[] {
+      const flexes: FlexName[] = [];
+      const count = Number.parseInt(this.number);
+      for (let i = 0; i < count; i++) {
+        for (const flex of this.names) {
+          flexes.push(flex);
+        }
+      }
+      return flexes;
+    }
+  }
+
+  // parse & track the state as we recurse through a flex sequence
+  class TrackState {
+    private readonly sequence: string;
+    private states: SequenceState[] = [];
+
+    constructor(sequence: string) {
+      this.sequence = sequence;
+      this.states.push(new SequenceState(0));
+    }
+
+    getNames(): FlexName[] {
+      let state = this.states[this.states.length - 1];
+      return state.names;
+    }
+
+    step(c: string) {
+      let state = this.states[this.states.length - 1];
+      if (c == '(') {
+        state = this.endFlex(state);
+        state = new SequenceState(state.index);
+        this.states.push(state);
+      } else if (c == ')') {
+        state = this.endFlex(state);
+        state.lookingForNumber = true;
+      } else if (state.lookingForNumber && ('0' <= c && c <= '9')) {
+        state.number += c;
+      }
+      else if (c === '>' || c === '<' || c === '^' || ('A' <= c && c <= 'Z')) {
+        // we're starting a new flex, so end the previous one
+        state = this.endFlex(state);
+        state.start = state.index;
+      }
+
+      state.index++;
+    }
+
+    done() {
+      for (let i = 0, len = this.states.length; i < len; i++) {
+        this.endFlex(this.states[this.states.length - 1]);
       }
     }
+
+    private endFlex(state: SequenceState): SequenceState {
+      if (state.lookingForNumber) {
+        const flexes = state.getFlexes();
+        const laststate = state;
+        this.states.pop();
+        state = this.states[this.states.length - 1];
+        for (const flex of flexes) {
+          state.names.push(flex);
+        }
+        state.index = laststate.index;
+        state.start = laststate.index;
+      } else {
+        this.addFlex(state);
+      }
+      return state;
+    }
+
+    private addFlex(state: SequenceState) {
+      if (state.start != -1) {
+        const substr = this.sequence.substring(state.start, state.index).trim();
+        if (substr.length > 0) {
+          state.names.push(makeFlexName(substr));
+        }
+      }
+    }
+
   }
 
 }
--- test/flex/testParseFlexSequence.ts
+++ test/flex/testParseFlexSequence.ts
@@ -19,6 +19,35 @@ namespace Flexagonator {
       expect(names[3].fullName).toBe("Sh'+");
       expect(names[4].fullName).toBe("P");
     });
+    it('can repeat flexes #1', () => {
+      const names = parseFlexSequence("(P* ^ >) x 2");
+      expect(names.length).toBe(6);
+      expect(names[0].fullName).toBe("P*");
+      expect(names[1].fullName).toBe("^");
+      expect(names[2].fullName).toBe(">");
+      expect(names[3].fullName).toBe("P*");
+      expect(names[4].fullName).toBe("^");
+      expect(names[5].fullName).toBe(">");
+    });
+    it('can repeat flexes #2', () => {
+      const names = parseFlexSequence("Ab(Cd>)x2Ef");
+      expect(names.length).toBe(6);
+      expect(names[0].fullName).toBe("Ab");
+      expect(names[1].fullName).toBe("Cd");
+      expect(names[2].fullName).toBe(">");
+      expect(names[3].fullName).toBe("Cd");
+      expect(names[4].fullName).toBe(">");
+      expect(names[5].fullName).toBe("Ef");
+    });
+    it('can repeat flexes #3', () => {
+      const names = parseFlexSequence("P (P >)2");
+      expect(names.length).toBe(5);
+      expect(names[0].fullName).toBe("P");
+      expect(names[1].fullName).toBe("P");
+      expect(names[2].fullName).toBe(">");
+      expect(names[3].fullName).toBe("P");
+      expect(names[4].fullName).toBe(">");
+    });
   });
 
 }
