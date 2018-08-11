c6755951094d460170564d5b33ff4bcc7acc569e
Scott Sherman <scott@loki3.com>
2018-08-11 16:56:23 -0700

    slight refactor of the API

--- src/draw/DrawPinchGraphAPI.ts
+++ src/draw/DrawPinchGraphAPI.ts
@@ -1,17 +1,22 @@
 namespace Flexagonator {
 
+  export interface DrawPinchOptions {
+    flexes?: string;    // draw the graph for a set of flexes in {P, <, >}
+    traverse?: string;  // draw the graph for the given Tuckerman traverse
+  }
+
   const traverseColor = "rgb(180, 180, 180)";
   const flexColor = "rgb(20, 100, 170)";
 
   // draw the graph described by a given series of flexes in {P, <, >},
   // and/or drawing the corresponding Tuckerman traverse
-  export function drawPinchGraph(canvas: string | HTMLCanvasElement, flexes?: string, traverse?: string) {
+  export function drawPinchGraph(canvas: string | HTMLCanvasElement, options: DrawPinchOptions) {
     const output: HTMLCanvasElement = canvas instanceof HTMLCanvasElement ? canvas : document.getElementById(canvas) as HTMLCanvasElement;
     const ctx = output.getContext("2d") as CanvasRenderingContext2D;
     ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
 
-    if (traverse) {
-      const traverseGraph = createPinchGraph(traverse);
+    if (options.traverse) {
+      const traverseGraph = createPinchGraph(options.traverse);
       if (isFlexError(traverseGraph)) {
         return traverseGraph;
       }
@@ -19,8 +24,8 @@ namespace Flexagonator {
       drawGraph(ctx, traverseGraph);
     }
 
-    if (flexes) {
-      const flexGraph = createPinchGraph(flexes);
+    if (options.flexes) {
+      const flexGraph = createPinchGraph(options.flexes);
       if (isFlexError(flexGraph)) {
         return flexGraph;
       }
