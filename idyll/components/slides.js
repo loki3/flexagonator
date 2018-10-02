1db27854d810a21d03603e7d2b8a45fe033c94a7
loki3 <loki3@users.noreply.github.com>
2018-10-01 21:34:56 -0700

    add buttons for stepping back & forth through the slides

--- idyll/components/slides.js
+++ idyll/components/slides.js
@@ -18,6 +18,30 @@ class Slides extends React.Component {
   constructor(props) {
     super(props);
     this.state = { current: props.start };
+    this.stepFirst = this.stepFirst.bind(this);
+    this.stepPrevious = this.stepPrevious.bind(this);
+    this.stepNext = this.stepNext.bind(this);
+    this.stepLast = this.stepLast.bind(this);
+  }
+
+  stepFirst() {
+    this.setState({ current: this.props.start });
+  }
+
+  stepPrevious() {
+    if (this.state.current > this.props.start) {
+      this.setState({ current: this.state.current - 1 });
+    }
+  }
+
+  stepNext() {
+    if (this.state.current < this.props.end) {
+      this.setState({ current: this.state.current + 1 });
+    }
+  }
+
+  stepLast() {
+    this.setState({ current: this.props.end });
   }
 
   getCurrentFile(props, state) {
@@ -28,7 +52,15 @@ class Slides extends React.Component {
     const { width, height, pattern } = this.props;
     const file = pattern.replace('*', this.state.current);
     return (
-      <img src={file} width={width} height={height} />
+      <div style={{ position: 'relative', width: width, height: height }}>
+        <img src={file} width={width} height={height} />
+        <div style={{ position: 'absolute', left: '0px', top: '0px' }}>
+          <button onClick={this.stepFirst}>&lt;&lt;</button>
+          <button onClick={this.stepPrevious}>&lt;</button>
+          <button onClick={this.stepNext}>&gt;</button>
+          <button onClick={this.stepLast}>&gt;&gt;</button>
+        </div>
+      </div >
     );
   }
 };
