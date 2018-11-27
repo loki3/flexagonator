3822bb74bca11e3a9fc17c2dae57f85f4bd35951
loki3 <loki3@users.noreply.github.com>
2018-11-26 22:10:22 -0800

    add copyright notice

---
+++ idyll/components/copyright.js
@@ -0,0 +1,7 @@
+"use strict";
+const React = require('react');
+
+/** Common copyright - unescaped to get around the tooling */
+const Copyright = props => <div>Copyright &copy; 2018 Scott Sherman</div>;
+
+module.exports = Copyright;
