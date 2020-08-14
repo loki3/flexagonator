5166aee0d3e385a63fa4a0dcbe8fc6c088b6f59a
Scott Sherman <scott@loki3.com>
2020-08-14 12:46:50 -0700

    drop the special code for "square hexadeca"
    it's now handled by the more general "square ring hexadecaflexagon" code

--- src/flex/NamePiecesToScript.ts
+++ src/flex/NamePiecesToScript.ts
@@ -126,11 +126,6 @@ namespace Flexagonator {
       }
     }
 
-    // square hexadeca
-    if (overallShape === 'square' && n === 16) {
-      const directions = repeat([true, true, false, false], 4);
-      return [{ angles: [67.5, 90] }, { directions }]
-    }
     // rhombic hexadeca
     if (overallShape === 'rhombic' && n === 16) {
       const directions = repeat([true, true, false, true, true, false, true, true], 2);
