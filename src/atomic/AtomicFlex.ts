685f2c9b2eba3f2001eb7c3de5fbcf256e0a4c54
Scott Sherman <scott@loki3.com>
2020-09-21 17:29:02 -0700

    start implementing AtomicFlex
    many cases supported, but not yet the special case of a hinge shift, which needs special handling for the direction its ignoring

---
+++ src/atomic/AtomicFlex.ts
@@ -0,0 +1,116 @@
+namespace Flexagonator {
+
+  /** definition of an "atomic flex", which transforms one atomic pattern into another */
+  export interface AtomicFlexDef {
+    readonly shorthand: string;
+    readonly name: string;
+    readonly input: string;  // AtomicPattern
+    readonly output: string; // AtomicPattern
+  }
+
+  export function makeAtomicFlex(def: AtomicFlexDef): AtomicFlex | AtomicParseError {
+    const input = stringToAtomicPattern(def.input);
+    if (isAtomicParseError(input)) {
+      return input;
+    }
+    const output = stringToAtomicPattern(def.output);
+    if (isAtomicParseError(output)) {
+      return output;
+    }
+    return new AtomicFlex(def.name, input, output);
+  }
+
+  /** info about how to apply a flex that uses AtomicPattern */
+  export class AtomicFlex {
+    constructor(
+      readonly name: string,
+      readonly pattern: AtomicPattern,
+      readonly output: AtomicPattern) {
+    }
+
+    createInverse(): AtomicFlex {
+      return new AtomicFlex("inverse " + this.name, this.output, this.pattern);
+    }
+
+    /** apply this flex to the given input */
+    apply(input: AtomicPattern): AtomicPattern | AtomicPatternError {
+      const matches = matchAtomicPattern(input, this.pattern);
+      if (isAtomicPatternError(matches)) {
+        return matches;
+      }
+
+      /*
+      console.log('');
+      console.log(JSON.stringify(matches));
+      console.log(JSON.stringify(this.output));
+      */
+
+      const otherLeft = this.getRemainder(this.pattern.otherLeft, matches.otherLeft, matches.otherRight);
+      const otherRight = this.getRemainder(this.pattern.otherRight, matches.otherLeft, matches.otherRight);
+
+      const left = this.makePats(this.output.left, matches.matches, matches.patsLeft);
+      if (isFlexError(left)) {
+        return { atomicPatternError: "PatMismatch" };
+      }
+      const right = this.makePats(this.output.right, matches.matches, matches.patsRight);
+      if (isFlexError(right)) {
+        return { atomicPatternError: "PatMismatch" };
+      }
+
+      return { otherLeft, left, right, otherRight, singleLeaf: false };
+    }
+
+    /** make a series of ConnectedPats by filling in 'output' with what's in 'matches' + 'more' */
+    private makePats(output: ConnectedPats | null, matches: Pat[], more?: ConnectedPats): ConnectedPats | null | FlexError {
+      if (output === null && more === undefined) {
+        return null;
+      }
+      const newPats: ConnectedPat[] = [];
+      if (output) {
+        for (let stack of output) {
+          const newPat = this.createPat(stack.pat.getAsLeafTree(), matches);
+          if (isFlexError(newPat)) {
+            return newPat;
+          }
+          newPats.push({ pat: newPat, direction: stack.direction });
+        }
+      }
+      if (more) {
+        more.map(m => newPats.push(m));
+      }
+      return newPats;
+    }
+
+    /** create a pat given a tree of indices into a set of matched pats */
+    private createPat(stack: LeafTree, matches: Pat[]): Pat | FlexError {
+      if (typeof (stack) === "number") {
+        const i = stack as number;
+        const pat = matches[Math.abs(i)];
+        return i > 0 ? pat.makeCopy() : pat.makeFlipped();
+      }
+      else if (Array.isArray(stack) && stack.length === 2) {
+        const a = this.createPat(stack[0], matches);
+        if (isFlexError(a)) {
+          return a;
+        }
+        const b = this.createPat(stack[1], matches);
+        if (isFlexError(b)) {
+          return b;
+        }
+        return combinePats(a, b);
+      }
+      return { reason: FlexCode.BadFlexOutput };
+    }
+
+    /** match reaminder pattern */
+    private getRemainder(pattern: Remainder, left: Remainder, right: Remainder): Remainder {
+      switch (pattern) {
+        case 'a': return left;
+        case '-a': return flipRemainder(left);
+        case 'b': return right;
+        case '-b': return flipRemainder(right);
+      }
+    }
+  }
+
+}
---
+++ test/atomic/testAtomicFlex.ts
@@ -0,0 +1,31 @@
+namespace Flexagonator {
+
+  describe('AtomicFlex.apply', () => {
+    it("should apply a simple flex", () => {
+      const toRight = makeAtomicFlex({ shorthand: 'X', name: 'X', input: 'a 1 > / [-3,2] < b', output: 'a 1 > 2 < / 3 < b' }) as AtomicFlex;
+      const input = stringToAtomicPattern('a 1 > 2 > / [4,3] < 5 < b') as AtomicPattern;
+      const result = toRight.apply(input);
+      if (isAtomicPatternError(result)) {
+        fail('failed to apply flex: ' + JSON.stringify(result));
+        return;
+      }
+      const asString = atomicPatternToString(result);
+      expect(asString).toBe('a 1 > 2 > 3 < / -4 < 5 < b');
+    });
+
+    /*
+    currently fails because it loses track of the direction of the matched pat, special case for single pat/ignore direction
+    it("should shift current hinge", () => {
+      const toRight = makeAtomicFlex({ shorthand: '>', name: '>', input: 'a / 1 b', output: 'a 1 / b' }) as AtomicFlex;
+      const input = stringToAtomicPattern('a 1 > 2 < / 3 > 4 < b') as AtomicPattern;
+      const result = toRight.apply(input);
+      if (isAtomicPatternError(result)) {
+        fail('failed to apply flex: ' + JSON.stringify(result));
+        return;
+      }
+      const asString = atomicPatternToString(result);
+      expect(asString).toBe('a 1 > 2 < 3 > / 4 < b');
+    });
+    */
+  });
+}
