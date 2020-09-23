05b5221424918c5c3dcce9e64e2c23cff873ebe2
Scott Sherman <scott@loki3.com>
2020-09-22 22:39:41 -0700

    simplify test writing for composing atomic flexes

--- test/atomic/testMakeAtomicFlexes.ts
+++ test/atomic/testMakeAtomicFlexes.ts
@@ -1,6 +1,35 @@
 namespace Flexagonator {
 
   describe('makeAtomicFlexes', () => {
+    it("should compose to create the exchange flex", () => {
+      testFormula("Ex", "Ur > ^ Ur' ^",
+        "a 1 > / [-3,2] > b",
+        "-a [2,-1] > / 3 > -b");
+    });
+
+    it("should compose to create the pocket flex", () => {
+      testFormula("K", "Ur > ^ Ur' > Ul ^",
+        "a [-2,1] > -3 > / [5,-4] > b",
+        "a 1 < 2 > / [-4,3] > -5 > -b");
+    });
+
+    // for flex 'name', test that 'flexes' applied to 'input' generates 'output'
+    function testFormula(name: string, flexes: string, input: string, output: string, log?: boolean): void {
+      const inputPattern = stringToAtomicPattern(input);
+      if (isAtomicParseError(inputPattern)) {
+        fail('bad input for flex ' + name + ' with error ' + JSON.stringify(inputPattern));
+        return;
+      }
+      const result = applyFlexes(inputPattern, flexes, log);
+      if (isAtomicPatternError(result)) {
+        fail('failed to apply flex ' + name + ' with error ' + JSON.stringify(result));
+        return;
+      }
+      const asString = atomicPatternToString(result);
+      expect(asString).toBe(output, "flex: " + name);
+    }
+
+    // apply a series of flexes to given input
     const atomics = makeAtomicFlexes();
     function applyFlexes(input: AtomicPattern, flexes: string, log?: boolean): AtomicPattern | AtomicPatternError {
       if (log) {
@@ -20,32 +49,6 @@ namespace Flexagonator {
       }
       return input;
     }
-
-    it("should compose to create the exchange flex", () => {
-      // Ex = Ur > ^ Ur' ^
-      // a 1 > / [-3,2] > b  ->  -a [2,-1] > / 3 > -b
-      const input = stringToAtomicPattern("a 1 > / [-3,2] > b") as AtomicPattern;
-      const result = applyFlexes(input, "Ur > ^ Ur' ^");
-      if (isAtomicPatternError(result)) {
-        fail('failed to apply flex: ' + JSON.stringify(result));
-        return;
-      }
-      const asString = atomicPatternToString(result);
-      expect(asString).toBe("-a [2,-1] > / 3 > -b");
-    });
-
-    it("should compose to create the pocket flex", () => {
-      // K = Ur > ^ Ur' > Ul ^
-      // a [-2,1] > -3 > / [5,-4] > b  ->  a 1 < 2 > / [-4,3] > -5 > -b
-      const input = stringToAtomicPattern("a [-2,1] > -3 > / [5,-4] > b") as AtomicPattern;
-      const result = applyFlexes(input, "Ur > ^ Ur' > Ul ^", false);
-      if (isAtomicPatternError(result)) {
-        fail('failed to apply flex: ' + JSON.stringify(result));
-        return;
-      }
-      const asString = atomicPatternToString(result);
-      expect(asString).toBe("a 1 < 2 > / [-4,3] > -5 > -b");
-    });
 
   });
 
