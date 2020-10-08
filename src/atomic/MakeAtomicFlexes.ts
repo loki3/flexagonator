0d6e2090220530bae143ddb17703da0011d45349
Scott Sherman <scott@loki3.com>
2020-10-07 22:37:50 -0700

    start from 1 for consistency

--- src/atomic/MakeAtomicFlexes.ts
+++ src/atomic/MakeAtomicFlexes.ts
@@ -41,7 +41,7 @@ namespace Flexagonator {
     flexes["Ul"] = makeAtomicFlex("unfold left", "a # [1,-2] / b", "a # 1 \\ 2 / -b") as AtomicFlex;
 
     flexes["Xr"] = makeAtomicFlex("exchange right", "a 1 \\ # [-3,2] \\ b", "-a [2,-1] \\ # 3 \\ -b") as AtomicFlex;
-    flexes["Xl"] = makeAtomicFlex("exchange left", "a 4 / # [5,-6] / b", "-a [-4,5] / # 6 / -b") as AtomicFlex;
+    flexes["Xl"] = makeAtomicFlex("exchange left", "a 1 / # [2,-3] / b", "-a [-1,2] / # 3 / -b") as AtomicFlex;
     flexes["K"] = makeAtomicFlex("pocket", "a [-2,1] \\ -3 \\ # [5,-4] \\ b", "a 1 / 2 \\ # [-4,3] \\ -5 \\ -b") as AtomicFlex;
 
     flexes["Hf"] = makeAtomicFlex("half: fold forward", "a [-2,1] \\ -3 \\ # [5,-4] \\ 6 \\ b", "a 1 / 2 \\ # [-4,3] \\ [-5,6] / b") as AtomicFlex;
