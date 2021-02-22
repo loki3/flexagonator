08e7590740fbf65896eff35587eb49a394a7762f
Scott Sherman <scott@loki3.com>
2021-02-21 17:31:40 -0800

    add the ability to create a new flex definition from a sequence

---
+++ src/flex/MakeFlexFromSequence.ts
@@ -0,0 +1,51 @@
+namespace Flexagonator {
+
+  /**
+   * Create a new flex definition from a flex sequence.
+   * Note: doesn't currently handle a + in the sequence.
+   */
+  export function makeFlexFromSequence(sequence: string, flexes: Flexes): Flex | FlexError {
+    const flexNames = parseFlexSequence(sequence);
+
+    const flexagon = makeEmptyFlexagon(flexes);
+    const fm = new FlexagonManager(flexagon);
+
+    const generator = makeGeneratingSequence(flexNames);
+    const result = fm.applyFlexes(generator, false);
+    if (isFlexError(result)) {
+      return result;
+    }
+    const output = fm.flexagon.getAsLeafTrees();
+
+    const undo = flexNames.map(flexName => flexName.getInverse()).reverse();
+    const result2 = fm.applyFlexes(undo, false);
+    if (isFlexError(result2)) {
+      return result2;
+    }
+    const input = fm.flexagon.getAsLeafTrees();
+
+    return makeFlex("new flex", input, output, FlexRotation.None);
+  }
+
+  function makeEmptyFlexagon(flexes: Flexes): Flexagon {
+    const allNames = Object.getOwnPropertyNames(flexes);
+    const someFlex = flexes[allNames[0]];
+    const numPats = someFlex.pattern.length;
+    const pats = [];
+    for (var i = 1; i <= numPats; i++) {
+      pats.push(i);
+    }
+    return Flexagon.makeFromTree(pats) as Flexagon;
+  }
+
+  function makeGeneratingSequence(flexNames: FlexName[]): FlexName[] {
+    return flexNames.map(flexName => {
+      if (flexName.baseName === '>' || flexName.baseName === '<'
+        || flexName.baseName === '^' || flexName.shouldGenerate) {
+        return flexName;
+      }
+      return makeFlexName(flexName.flexName + '*');
+    });
+  }
+
+}
---
+++ test/flex/testMakeFlexFromSequence.ts
@@ -0,0 +1,17 @@
+namespace Flexagonator {
+
+  describe('makeFlexFromSequence', () => {
+    const flexes = makeAllFlexes(6);
+
+    it("should create Lh from LtbT'", () => {
+      const result = makeFlexFromSequence("Ltb T'", flexes);
+      if (isFlexError(result)) {
+        fail("failed to make flex");
+        return;
+      }
+      expect(JSON.stringify(result.pattern)).toBe("[[[1,[7,8]],9],2,3,4,[[5,11],10],6]");
+      expect(JSON.stringify(result.output)).toBe("[[[6,-9],-11],1,-8,[-2,7],[[4,-10],-3],5]");
+    })
+  });
+
+}
