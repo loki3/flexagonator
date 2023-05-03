3364c9dd2cf4ad0f9fb47efcea26ad513b47a455
Scott Sherman <scott@loki3.com>
2023-05-03 15:28:00 -0700

    use Paint from drawPinchGraph

--- src/draw/DrawPinchGraphAPI.ts
+++ src/draw/DrawPinchGraphAPI.ts
@@ -6,18 +6,20 @@ namespace Flexagonator {
     readonly drawEnds?: boolean; // show where 'flexes' starts & ends
   }
 
-  const traverseColor = "rgb(180, 180, 180)";
-  const flexColor = "rgb(20, 100, 170)";
-  const startColor = "rgb(20, 200, 50)";
-  const endColor = "rgb(200, 20, 100)";
+  const traverseColor = 0xb4b4b4;
+  const flexColor = 0x1464aa;
+  const startColor = 0x14c832;
+  const endColor = 0xc81464;
 
   // draw the graph described by a given series of flexes in {P, P', ^, <, >},
   // and/or drawing the corresponding Tuckerman traverse
   export function drawPinchGraph(canvas: string | HTMLCanvasElement, options: DrawPinchOptions) {
     const output: HTMLCanvasElement = canvas instanceof HTMLCanvasElement ? canvas : document.getElementById(canvas) as HTMLCanvasElement;
     const ctx = output.getContext("2d") as CanvasRenderingContext2D;
-    const box = { x: ctx.canvas.clientWidth, y: ctx.canvas.clientHeight };
-    ctx.clearRect(0, 0, box.x, box.y);
+    const paint = new PaintCanvas(ctx);
+    const [w, h] = paint.getSize();
+    const box = { x: w, y: h };
+    paint.start();
 
     var transform = null;
     if (options.traverse) {
@@ -27,12 +29,12 @@ namespace Flexagonator {
       }
       transform = Transform.make(box, traverseGraph.min, traverseGraph.max, false);
 
-      ctx.strokeStyle = traverseColor;
-      drawGraph(ctx, transform, traverseGraph);
+      paint.setLineColor(traverseColor);
+      drawGraph(paint, transform, traverseGraph);
 
       if (options.drawEnds && traverseGraph.points.length > 0) {
-        ctx.strokeStyle = startColor;
-        drawCircle(ctx, transform, traverseGraph.points[0], 0.1);
+        paint.setLineColor(startColor);
+        drawCircle(paint, transform, traverseGraph.points[0], 0.1);
       }
     }
 
@@ -45,32 +47,27 @@ namespace Flexagonator {
         transform = Transform.make(box, flexGraph.min, flexGraph.max, false);
       }
 
-      ctx.strokeStyle = flexColor;
-      drawGraph(ctx, transform, flexGraph);
+      paint.setLineColor(flexColor);
+      drawGraph(paint, transform, flexGraph);
 
       if (options.drawEnds && flexGraph.points.length > 0) {
-        ctx.strokeStyle = endColor;
-        drawCircle(ctx, transform, flexGraph.points[flexGraph.points.length - 1], 0.09);
+        paint.setLineColor(endColor);
+        drawCircle(paint, transform, flexGraph.points[flexGraph.points.length - 1], 0.09);
       }
     }
+    paint.end();
     return true;
   }
 
-  function drawGraph(ctx: CanvasRenderingContext2D, transform: Transform, graph: PinchGraph) {
-    ctx.beginPath();
-    for (const point of graph.points) {
-      const p = transform.apply(point);
-      ctx.lineTo(p.x, p.y);
-    }
-    ctx.stroke();
+  function drawGraph(paint: Paint, transform: Transform, graph: PinchGraph) {
+    const points = graph.points.map(p => transform.apply(p));
+    paint.drawLines(points);
   }
 
-  function drawCircle(ctx: CanvasRenderingContext2D, transform: Transform, p: Point, radius: number) {
+  function drawCircle(paint: Paint, transform: Transform, p: Point, radius: number) {
     const center = transform.apply(p);
     const size = transform.applyScale(radius);
-    ctx.beginPath();
-    ctx.ellipse(center.x, center.y, size, size, 0, 0, Math.PI * 2);
-    ctx.stroke();
+    paint.drawCircle(center, size);
   }
 
 }
