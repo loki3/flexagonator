da4f09dd26b371b74a84c0bc1b930e48828111a1
Scott Sherman <scott@loki3.com>
2023-09-16 22:08:07 -0700

    make morph kite flexes work with directions

--- src/atomic/MakeMorphFlexes.ts
+++ src/atomic/MakeMorphFlexes.ts
@@ -2,9 +2,8 @@ namespace Flexagonator {
 
   /**
    * create local flexes that transform 4 pats
-   * from an arrangement where all pats meet in the middle (\\\\)
-   * to an arrangement where there's a "kite" sticking off (/\\/).
-   * NOTE: these really should include 'directions' in the flex definitions
+   * from an arrangement where all pats meet in the middle (////)
+   * to an arrangement where there's a "kite" sticking off (\//\).
    */
   export function makeMorphFlexes(patCount: number): Flexes {
     let flexes: Flexes = {};
@@ -12,25 +11,31 @@ namespace Flexagonator {
       return flexes;
     }
 
-    // flexes from main position to kite position
-    flexes["Mkb"] = createMorphFlex("morph-kite: fold back", patCount - 4, 7,
+    // flexes from main position to kite position: //// -> \//\
+    flexes["Mkb"] = createLocalFlex("morph-kite: fold back", patCount - 4, 7,
       [1, [-3, 2]], /**/[-4, [6, -5]],  // 1 2 1 2
-      [[1, -2], [4, -3]], /**/[5, 6]);  // 2 2 1 1
-    flexes["Mkf"] = createMorphFlex("morph-kite: fold forward", patCount - 4, 7,
+      [[1, -2], [4, -3]], /**/[5, 6],   // 2 2 1 1
+      "//", "//", "|/", "/|", FlexRotation.BCA);
+    flexes["Mkf"] = createLocalFlex("morph-kite: fold forward", patCount - 4, 7,
       [[-2, 1], -3], /**/[[5, -4], 6],  // 2 1 2 1
-      [1, 2], /**/[[-4, 3], [-5, 6]]);  // 1 1 2 2
-    flexes["Mkl"] = createMorphFlex("morph-kite: fold left back", patCount - 4, 7,
+      [1, 2], /**/[[-4, 3], [-5, 6]],   // 1 1 2 2
+      "//", "//", "|/", "/|", FlexRotation.BCA);
+    flexes["Mkl"] = createLocalFlex("morph-kite: fold left back", patCount - 4, 7,
       [[-2, 1], -3], /**/[-4, [6, -5]], // 2 1 1 2
-      [1, [4, [2, -3]]], /**/[5, 6]);   // 1 -3 1 1
-    flexes["Mkr"] = createMorphFlex("morph-kite: fold right forward", patCount - 4, 7,
+      [1, [4, [2, -3]]], /**/[5, 6],    // 1 -3 1 1
+      "//", "//", "|/", "/|", FlexRotation.BCA);
+    flexes["Mkr"] = createLocalFlex("morph-kite: fold right forward", patCount - 4, 7,
       [[-2, 1], -3], /**/[-4, [6, -5]], // 2 1 1 2
-      [1, 2], /**/[[[-4, 5], 3], 6]);   // 1 1 3 1
-    flexes["Mkfs"] = createMorphFlex("morph-kite: front shuffle", patCount - 4, 8,
+      [1, 2], /**/[[[-4, 5], 3], 6],     // 1 1 3 1
+      "//", "//", "|/", "/|", FlexRotation.BCA);
+    flexes["Mkfs"] = createLocalFlex("morph-kite: front shuffle", patCount - 4, 8,
       [1, [[-3, 4], 2]], /**/[5, [-7, 6]],      // 1 3 1 2
-      [[1, -2], -3], /**/[[[5, -6], -4], -7]);  // 2 1 3 1
-    flexes["Mkbs"] = createMorphFlex("morph-kite: back shuffle", patCount - 4, 8,
+      [[1, -2], -3], /**/[[[5, -6], -4], -7],   // 2 1 3 1
+      "//", "//", "|/", "/|", FlexRotation.BCA);
+    flexes["Mkbs"] = createLocalFlex("morph-kite: back shuffle", patCount - 4, 8,
       [[-2, 1], -3], /**/[[-6, [-4, 5]], -7],   // 2 1 -3 1
-      [1, [4, [2, -3]]], /**/[5, [6, -7]]);     // 1 -3 1 2
+      [1, [4, [2, -3]]], /**/[5, [6, -7]],      // 1 -3 1 2
+      "//", "//", "|/", "/|", FlexRotation.BCA);
 
     // only works on a hexa
     if (patCount === 6) {
@@ -45,10 +50,10 @@ namespace Flexagonator {
     }
 
     // flexes that go between kite positions
-    flexes["Sp"] = createMorphFlex("partial shuffle", patCount - 5, 8,
+    flexes["Sp"] = createLocalFlex("partial shuffle", patCount - 5, 8,
       [1, 2, [-4, 3]], /**/[-5, [-6, 7]],
       [[1, -2], -3], /**/[[5, -4], 6, 7]);
-    flexes["Lkk"] = createMorphFlex("kite-to-kite slot", patCount - 6, 9,
+    flexes["Lkk"] = createLocalFlex("kite-to-kite slot", patCount - 6, 9,
       [1, 2, 3, 4], /**/[5, [[-7, 6], 8]],
       [[1, [3, -2]], 4], /**/[5, 6, 7, 8]);
 
@@ -60,21 +65,37 @@ namespace Flexagonator {
     return flexes;
   }
 
-  function createMorphFlex(name: string, patsNeeded: number, nextId: number,
-    inLeft: LeafTree[], inRight: LeafTree[], outLeft: LeafTree[], outRight: LeafTree[]
+  function createLocalFlex(name: string, patsNeeded: number, nextId: number,
+    inLeft: LeafTree[], inRight: LeafTree[], outLeft: LeafTree[], outRight: LeafTree[],
+    inDirLeft?: string, inDirRight?: string, outDirLeft?: string, outDirRight?: string,
+    rotation?: FlexRotation
   ): Flex {
     const input = createLeafTree(patsNeeded, nextId, inLeft, inRight);
     const output = createLeafTree(patsNeeded, nextId, outLeft, outRight);
-    return makeFlex(name, input, output, FlexRotation.None) as Flex;
+    const inputDirs = createDirectionsOpt(patsNeeded, inDirLeft, inDirRight);
+    const outputDirs = createDirectionsOpt(patsNeeded, outDirLeft, outDirRight);
+    const fr = rotation === undefined ? FlexRotation.None : rotation;
+    return makeFlex(name, input, output, fr, inputDirs, outputDirs) as Flex;
   }
 
-  // make [<right>, nextId, nextId+1..., <left>] so current hinge is between left & right
+  /** make [<right>, nextId, nextId+1..., <left>] so current hinge is between left & right */
   function createLeafTree(patsNeeded: number, nextId: number, left: LeafTree[], right: LeafTree[]): LeafTree[] {
     let pats: LeafTree[] = right;
     for (let i = nextId; i < nextId + patsNeeded; i++) {
       pats = pats.concat(i);
     }
     return pats.concat(left);
+  }
+
+  /** flex out the directions for the entire flexagon: right + ?'s + left */
+  function createDirectionsOpt(
+    patsNeeded: number, inDirLeft?: string, inDirRight?: string
+  ): string | undefined {
+    if (inDirLeft === undefined || inDirRight === undefined) {
+      return undefined;
+    }
+    const dirs = inDirRight + '?'.repeat(patsNeeded) + inDirLeft;
+    return dirs;
   }
 
 }
