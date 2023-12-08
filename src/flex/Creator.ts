e07c6a387132290141621f6697ee118eea77432b
Scott Sherman <scott@loki3.com>
2023-12-08 13:08:29 -0800

    better encapsulate creation behavior

---
+++ src/flex/Creator.ts
@@ -0,0 +1,109 @@
+namespace Flexagonator {
+
+  /** simplify various tasks around creating flexagons & figuring out their properties */
+  export class Creator {
+    private fm: FlexagonManager;
+    private pieces: NamePieces;
+    private generator?: string;
+    private pats?: LeafTree[];
+
+    /** pass colors to use when creating flexagons, initializes a default hexa-hexaflexagon */
+    constructor(private readonly colors: number[]) {
+      this.pieces = { patsPrefix: 6 };
+      const script: ScriptItem[] = [
+        { pats: [[0, 0], [[0, 0], [0, 0]], [0, 0], [[0, 0], [0, 0]], [0, 0], [[0, 0], [0, 0]]] },
+        { normalizeIds: true, labelAsTree: colors }
+      ];
+      this.fm = createFromScript(script) as FlexagonManager;
+    }
+
+    getFlexagonManager(): FlexagonManager {
+      return this.fm;
+    }
+
+    /** get the name of the current flexagon */
+    getName() {
+      return namePiecesToName(this.pieces);
+    }
+
+    /** get the directions between pats */
+    getDirections(): string {
+      const dirs = this.fm.getDirections();
+      return !dirs ? '' : dirs.asString(false);
+    }
+
+    /** get a list of all supported flexes */
+    getSupportedFlexes() {
+      // TBD
+    }
+
+    runScriptItem(script: ScriptItem): true | FlexError | TreeError {
+      const result = runScriptItem(this.fm, script);
+      if (isError(result)) {
+        return result;
+      }
+      return true;
+    }
+
+    /** use the given name pieces to generate subsequent flexagons */
+    setNamePieces(pieces: NamePieces): boolean | TreeError | FlexError {
+      if (namePiecesToScript(pieces)[1].length > 0) {
+        return false; // not enough pieces to determine flexagon
+      }
+      this.pieces = pieces;
+      this.generator = undefined;
+      this.pats = undefined;
+      return this.newFlexagon();
+    }
+
+    /** create a new flexagon from a flex sequence */
+    createFromSequence(flexes: string): boolean | TreeError | FlexError {
+      this.generator = flexes;
+      this.pats = undefined;
+      return this.newFlexagon();
+    }
+
+    /** create a new flexagon from a description of the pats to use */
+    createFromPats(rawPats: string): boolean | TreeError | FlexError {
+      try {
+        const parsed = JSON.parse(rawPats);
+        if (!Array.isArray(parsed)) {
+          return { reason: TreeCode.ExpectedArray, context: rawPats };
+        }
+        this.pats = parsed;
+        return this.newFlexagon();
+      } catch (e) {
+        return { reason: TreeCode.ExpectedArray, context: rawPats };
+      }
+    }
+
+    /**
+     * create a flexagon given the current creation setting
+     * @returns true for success, false if name is incomplete, or specific error
+     */
+    private newFlexagon(): boolean | TreeError | FlexError {
+      // use name pieces if complete enough
+      const [script, errors] = namePiecesToScript(this.pieces);
+      if (errors.length > 0) {
+        return false; // insufficient info, so nothing changed
+      }
+
+      // build up complete script for creating flexagon
+      if (this.generator) {
+        script.push({ flexes: this.generator, history: 'clear' });
+      } else if (this.pats) {
+        script.push({ pats: this.pats });
+      }
+      script.push({ labelAsTree: this.colors }); // label & color
+
+      // create flexagon
+      const result = Flexagonator.createFromScript(script);
+      if (Flexagonator.isError(result)) {
+        return result;  // report specific error
+      }
+      this.fm = result;
+      return true;  // success
+    }
+  }
+
+}
---
+++ test/flex/testCreator.ts
@@ -0,0 +1,38 @@
+namespace Flexagonator {
+
+  describe('Creator', () => {
+    it('reports the current name', () => {
+      const creator = new Creator([]);
+      creator.setNamePieces({ overallShape: 'kite', leafShape: 'bronze', patsPrefix: 8 });
+      const name = creator.getName();
+      expect(name).toBe('kite bronze octaflexagon');
+    });
+
+    it('keeps current flexagon if name is incomplete', () => {
+      const creator = new Creator([]);
+      const result = creator.setNamePieces({ overallShape: 'kite', leafShape: 'bronze', patsPrefix: 8 });
+      expect(result).toBe(true);
+      const result2 = creator.setNamePieces({ overallShape: 'pentagonal' });
+      expect(result2).toBe(false);
+      const name = creator.getName();
+      expect(name).toBe('kite bronze octaflexagon');
+    });
+
+    it('uses a generating sequence and can apply flexes', () => {
+      const creator = new Creator([]);
+      creator.setNamePieces({ patsPrefix: 9 });
+      const result = creator.createFromSequence("S+");
+      expect(result).toBe(true);
+      creator.runScriptItem({ flexes: "S" });
+      expect(creator.getFlexagonManager().getTotalStates()).toBe(2);
+    });
+
+    it('complains about bad pats', () => {
+      const creator = new Creator([]);
+      creator.setNamePieces({ patsPrefix: 9 });
+      const result = creator.createFromPats('invalid') as TreeError;
+      expect(result.reason).toBe('expected array');
+    });
+  });
+
+}
