321e124cacd3a4f61242b44fcdb80af0500e8c7e
Scott Sherman <loki3@users.noreply.github.com>
2026-03-22 18:18:10 -0700

    convert atomic pat notation to a flex

---
+++ src/atomic/ConvertAtomicFlex.ts
@@ -0,0 +1,43 @@
+namespace Flexagonator {
+
+  /** convert a flex specified in atomic pat notation to a Flex object */
+  export function convertAtomicFlex(
+    name: string, patCount: number,
+    inPattern: string, outPattern: string, rotation?: FlexRotation
+  ): Flex | AtomicParseError {
+    const atomicFlex = makeAtomicFlex(name, inPattern, outPattern);
+    if (isAtomicParseError(atomicFlex)) {
+      return atomicFlex;
+    }
+
+    const [inPatLeft, inDirLeft] = convertPats(atomicFlex.input, 'left');
+    const [inPatRight, inDirRight] = convertPats(atomicFlex.input, 'right');
+    const [outPatLeft, outDirLeft] = convertPats(atomicFlex.output, 'left');
+    const [outPatRight, outDirRight] = convertPats(atomicFlex.output, 'right');
+
+    const modifiedCount = getPatsCount(atomicFlex.input.left) + getPatsCount(atomicFlex.input.right);
+    const totalLeaves = getLeafCount(atomicFlex.input.left) + getLeafCount(atomicFlex.input.right);
+
+    const flex = createLocalFlex(name, patCount - modifiedCount, totalLeaves,
+      inPatLeft, inPatRight, outPatLeft, outPatRight,
+      inDirLeft, inDirRight, outDirLeft, outDirRight, rotation);
+    return flex;
+  }
+
+  function convertPats(pattern: AtomicPattern, side: 'left' | 'right'): [LeafTree[], string] {
+    const connected = side === 'left'
+      ? pattern.left?.slice().reverse()
+      : pattern.right;
+    if (connected === null || connected === undefined) {
+      return [[], ''];
+    }
+
+    const trees: LeafTree[] = [];
+    let dirs = '';
+    for (const cp of connected) {
+      trees.push(cp.pat.getAsLeafTree());
+      dirs += cp.direction;
+    }
+    return [trees, dirs];
+  }
+}
---
+++ test/atomic/testConvertAtomicFlex.ts
@@ -0,0 +1,64 @@
+namespace Flexagonator {
+
+  describe('convertAtomicFlex', () => {
+    function checkForEqual(flex: Flex, atomicFlex: Flex): boolean {
+      const leaftree = flex.input;
+      const dirs = createDirs(atomicFlex.inputDirs);
+      const pre = Flexagon.makeFromTree(leaftree, undefined, dirs) as Flexagon;
+      const result1 = applyFlex(flex, pre);
+      const result2 = applyFlex(atomicFlex, pre);
+      return result1.isSameState(result2);
+    }
+    function createDirs(dirs: DirectionsOpt | undefined): Directions | undefined {
+      if (dirs === undefined) {
+        return undefined;
+      }
+      const bools = dirs.asRaw().map(d => d === null ? true : d);
+      return Directions.make(bools);
+    }
+    function applyFlex(flex: Flex, flexagon: Flexagon): Flexagon {
+      const result = flex.apply(flexagon);
+      if (isFlexError(result)) {
+        fail('failed to apply flex');
+        return flexagon;
+      }
+      return result;
+    }
+
+    it('supports the atomic pat version of the pyramid shuffle', () => {
+      const atomicFlex = convertAtomicFlex('S', 7,
+        'a [[[3,-2],-4],1] / -5 / # [7,-6] / b', 'a [-2,1] / -3 / # [7,[-4,[-6,5]]] / b');
+      if (isAtomicParseError(atomicFlex)) {
+        fail('failed to make pyramid shuffle');
+        return;
+      }
+      const flexes: Flexes = makeAllFlexes(7);
+
+      expect(checkForEqual(flexes['S'], atomicFlex));
+    });
+
+    it('supports the atomic pat version of the Tao flex', () => {
+      const atomicFlex = convertAtomicFlex('Tao', 10,
+        'a [10,-9] / 11 \\ 12 / # [-2,1] / -3 / -4 / -5 \\ b',
+        'a -9 \\ -10 / # -11 / [1,-12] / 2 / 3 \\ [-5,4] / b');
+      if (isAtomicParseError(atomicFlex)) {
+        fail('failed to make Tao flex' + JSON.stringify(atomicFlex));
+        return;
+      }
+      const flexes: Flexes = makeMorphFlexes(10);
+
+      expect(checkForEqual(flexes['Tao'], atomicFlex));
+    });
+
+    it('complains about bad input', () => {
+      const error = convertAtomicFlex('Err', 6,
+        'a [10,-9] / # 5',
+        'a [-2,1] / -3 / # [7,[-4,[-6,5]]] / b');
+      if (!isAtomicParseError(error)) {
+        fail('expected an error' + JSON.stringify(error));
+        return;
+      }
+      expect(error.atomicParseCode).toBe('MissingOtherRight');
+    });
+  });
+}
